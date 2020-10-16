782445f79858a662a705dc16a429acc188335467
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 10:22:02 +0700

    fix

---
+++ src/if1/pkg10119006/latihan14/IF110119006Latihan14.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan14;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil bit
+ */
+public class IF110119006Latihan14 {
+
+    public static void main(String[] args) {
+        int a = 60; /* 69 = 011 1100 */
+        int b = 13; /* 13 = 000 1101 */
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /*61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /*-61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+    
+}
